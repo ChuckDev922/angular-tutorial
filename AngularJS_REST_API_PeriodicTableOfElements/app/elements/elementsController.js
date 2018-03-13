a48054b391b4259913f9124736755f427749bdac
ChuckDev922 <cepeasley@earthlink.net>
2018-03-13 10:52:05 -0700

    Add files via upload

---
+++ AngularJS_REST_API_PeriodicTableOfElements/app/elements/elementsController.js
@@ -0,0 +1,41 @@
+﻿(function () {
+	// fetch all elements
+	var elementsController = function ($scope, $filter, remoteElementsService) {
+	    $scope.fetchElements = function () {
+            remoteElementsService.fetchElements()
+			.success(function (data, status, headers, config) {
+			    $scope.elements = data;
+			})
+			.error(function (data, status, headers, config) {
+			    $scope.elements = null;
+			    $scope.error = "Failed to retrieve item from server: id=";
+			});
+	    };
+
+		// fetch element by ID
+		$scope.fetchElement = function (id) {
+			
+			if (id == -1) {
+				alert('See Lanthanides section');
+				return;
+			}
+			else if (id == -2) {
+				alert('See Actinides section');
+				return;
+			}
+			console.log('elementsController: element id = ' + id);
+			remoteElementsService.fetchElement(id)
+			.success(function (data, status, headers, config) {
+				$scope.singleElement = data;
+				console.log('elementsController success: element id = ' + $scope.singleElement.ID);
+			})
+			.error(function (data, status, headers, config) {
+				$scope.singleElement = null;
+				$scope.error = "Failed to retrieve item from server: id=" + id;
+			});
+		};
+		
+	}
+
+	angular.module('myAngularApplicationElements').controller('elementsController', ["$scope", "$filter", "remoteElementsService",  elementsController]);
+}());
---
+++ AngularJS_REST_API_PeriodicTableOfElements/app/elements/remoteElementsService.js
@@ -0,0 +1,21 @@
+﻿(function () {
+	var remoteElementsService = function ($http) {
+		
+		var _fetchElements = function () {
+		    console.log('service: url = ' + 'http://localhost:64227/api/Element');
+		    return $http.get('http://localhost:64227/api/Element');
+		};
+
+		var _fetchElement = function (id) {
+		    console.log('service: url = ' + 'http://localhost:64227/api/Element/' + id);
+			return $http.get('http://localhost:64227/api/Element/' + id);
+        };
+		
+        return {
+            fetchElements:             _fetchElements, // remember this comma
+			fetchElement:				_fetchElement
+        };
+    }
+
+    angular.module('myAngularApplicationElements').factory('remoteElementsService', ["$http", remoteElementsService]);
+}());
